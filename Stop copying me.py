0719b00f65ce144991c130de6618b68d9820303e
Joseph Daniel Olawale <missionarypwg@gmail.com>
2019-04-01 23:14:59 +0100

    Add files via upload

---
+++ Stop copying me.py
@@ -0,0 +1,11 @@
+msg = input('Please say something: ')
+
+while msg != 'stop copying me':
+    print(msg)
+    msg = input()
+print('UGH, You Win')
+
+
+# while msg != 'stop copying me':
+#     msg = input(f'{msg} ')
+# print('UGH, You Win')
